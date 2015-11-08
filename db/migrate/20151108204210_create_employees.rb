77ce9c0054c3bf1f8a34a8b6712aaed34247cfd3
Stanley <stanleyjsdz@gmail.com>
2015-11-08 14:04:14 -0700

    create and run migration for employees

---
+++ db/migrate/20151108204210_create_employees.rb
@@ -0,0 +1,9 @@
+class CreateEmployees < ActiveRecord::Migration
+  def change
+    create_table :employees do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+    end
+  end
+end
