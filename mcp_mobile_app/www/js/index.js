b3124babddacc231418cc4018b5d794adb8dc277
Stanley <stanleyjsdz@gmail.com>
2015-11-11 12:18:30 -0700

    add scripts
    Jquery now shows a users dashboard on login and loads their name in the head

--- mcp_mobile_app/www/js/index.js
+++ mcp_mobile_app/www/js/index.js
@@ -1,26 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-    $(document).bind("mobileinit", function() {
-      $.mobile.allowCrossDomainPages = true;
-      $.support.cors = true
-    });
-
 var app = {
     // Application Constructor
     initialize: function() {
@@ -54,6 +31,11 @@ var app = {
 
 };
 
+$(document).bind("mobileinit", function() {
+  $.mobile.allowCrossDomainPages = true;
+  $.support.cors = true
+});
+
 app.initialize();
 
 $(document).ready(function() {
@@ -68,8 +50,15 @@ $(document).ready(function() {
       }, 
 
       success: function(data) {
+        $('.app-home').hide(),	  
+        $('.app-dash').show(),
+        renderEmployeeName(data),
 	console.log(data["first_name"])
       }
     })
   })
 });
+
+renderEmployeeName = function(data) {
+  $('.employee-name').text(data["first_name"])
+};
