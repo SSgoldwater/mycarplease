21ed39f979a45ead6ed950cc1068ecdd1eed1ecb
Stanley <stanleyjsdz@gmail.com>
2015-11-12 14:30:08 -0700

    return status to api calls

--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::VehiclesController < ApiController
 
   def create
     location = Location.find_by(name: params["account"])
-    new_vehicle = location.vehicles.create(ticket_no: params["ticketNo"], space: params["space"])
+    new_vehicle = location.vehicles.create(ticket_no: params["ticketNo"], space: params["space"], status: "parked")
     respond_with new_vehicle, location: root_url
   end
 
