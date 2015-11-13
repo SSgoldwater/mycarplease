b208d605982c0ebdc95a23c094ab80629ab9c993
Stanley <stanleyjsdz@gmail.com>
2015-11-12 20:43:21 -0700

    add methods for customer flow

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,6 +5,23 @@ class CustomersController < ApplicationController
   end
 
   def create
+    @customer = Customer.create(phone: customer_params[:phone])
+
+    render :get_vehicle
+  end
+
+  def update
+    customer = Customer.find(params[:id])
+    customer.location = Location.find_by(name: params["customer"]["location"])
+
+    customer.update!(customer_params)
+    render :final
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:phone, :ticket_no, :text_confirmation)
   end
 
 end
