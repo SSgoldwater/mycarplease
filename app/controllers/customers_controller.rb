b32a70bc2c5aa958beaa1271f8ea11bb7db85303
Stanley <stanleyjsdz@gmail.com>
2015-11-13 19:06:08 -0700

    added methods for sending text confirmation code

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,6 +6,7 @@ class CustomersController < ApplicationController
 
   def create
     @customer = Customer.create(phone: customer_params[:phone])
+    @customer.send_code(params["customer"]["phone"])
 
     render :get_vehicle
   end
@@ -13,7 +14,10 @@ class CustomersController < ApplicationController
   def update
     customer = Customer.find(params[:id])
     customer.location = Location.find_by(name: params["customer"]["location"])
-
+    binding.pry
+    if customer.verify
+      customer.vehicle = Vehicle.find_by(ticket_no: params["customer"]["ticket_no"]
+    end
     customer.update!(customer_params)
     render :final
   end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,17 @@
 class Customer < ActiveRecord::Base
   belongs_to :location
+  has_one    :vehicle
+
+  def send_code(phone_number)
+    code = "123 456"
+    self.text_confirmation = code
+    binding.pry
+    self.save
+    Messenger.send_text(phone_number, code)
+  end
+
+  def verify(customer, customer_params)
+    if customer.text_confirmation == customer_params[:text_confirmation] then true
+  end
+
 end
