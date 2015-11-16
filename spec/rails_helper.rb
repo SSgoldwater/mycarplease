dcffd368a335577614e43d9b3c0e3724575ab7db
Stanley <stanleyjsdz@gmail.com>
2015-11-15 17:04:00 -0700

    added simplecov starter

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,8 +5,11 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'capybara'
+require 'capybara/rspec'
 require 'database_cleaner'
+require 'simplecov'
+
+SimpleCov.start
 
 ActiveRecord::Migration.maintain_test_schema!
 
