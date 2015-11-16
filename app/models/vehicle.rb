1688121ce0af13e3998400d8b223613e4ae07d42
Stanley <stanleyjsdz@gmail.com>
2015-11-15 17:02:41 -0700

    added validations to vehicle model

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,12 @@
 class Vehicle < ActiveRecord::Base
   belongs_to :location
   belongs_to :customer
+  validates :ticket_no, length: { is: 3 }
+  validates :space, presence: true
+  before_save :default_values
+  
+  def default_values
+    self.status ||= "parked"
+  end
+
 end
