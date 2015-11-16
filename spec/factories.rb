6771a5bc6e8b7e3ff1757746f76328f9c021ff19
Stanley <stanleyjsdz@gmail.com>
2015-11-15 17:03:26 -0700

    added factories required by specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,32 @@
 FactoryGirl.define do
+
   factory :customer do
     phone "4196997844"
-    name "Stanley"
+    name "John"
     ticket_no 101
     text_confirmation 1234
     location_id 1 
   end
+
+  factory :employee do
+    first_name "Stanley"
+    last_name "Ski"
+    email "stanley@gmail.com"
+    password "password"
+    status "inactive"
+  end 
+  
+  factory :location do
+    name "Chloe" 
+  end
+   
+  factory :vehicle do
+    color "red"
+    style "sedan"
+    ticket_no 101
+    space "G15"
+    location_id 1
+    status "parked"
+  end 
+
 end
