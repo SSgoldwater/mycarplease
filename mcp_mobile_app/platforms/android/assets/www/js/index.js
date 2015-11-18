7bbaed8438923e35a029e2ef71009c7da1d313ef
Stanley <stanleyjsdz@gmail.com>
2015-11-18 07:58:13 -0700

    cordova build

--- mcp_mobile_app/platforms/android/assets/www/js/index.js
+++ mcp_mobile_app/platforms/android/assets/www/js/index.js
@@ -42,10 +42,6 @@ app.initialize();
 $(document).ready(function() {
 
   $('.clock-in-button').on('click', function() {
-    cordova.plugins.notification.local.schedule({
-	  id: 1,
-	text: "You have a customer waiting for a quote.",
-    });
     $.ajax({
       type: 'POST',
       url: 'http://mycarplease.herokuapp.com/api/v1/clockin',
