8c25cbc7124d0854a2907011d44a4c6aea4d2556
Stanley <stanleyjsdz@gmail.com>
2015-11-19 08:31:39 -0700

    customer model at 100% coverage

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -20,5 +20,30 @@ RSpec.describe Customer, type: :model do
     expect(customer.save).to be true
   end
 
+  it "has a send_code method that sends a successful twilio code" do
+    sms = customer.send_code(customer.phone)
+    expect(sms.body).to include("confirmation number")
+  end
+  
+  it "has a send_quote method that sends a message with quote time" do
+    sms = customer.send_quote(10)
+    expect(sms.body).to include("Your vehicle will be ready in 10 minutes.")
+  end
+
+  it "has a verify method that verifies the customer by text confirmation" do
+    vehicle = create(:vehicle)
+    create(:location)
+    params = { "customer" => { "location" => 1 , "ticket_no" => 101 } }
+    customer_params = { text_confirmation: "1234" } 
+
+    expect(customer.verify(customer, params, customer_params)).to match(:final)
+
+    customer_params = { text_confirmation: "1111" } 
+    expect(customer.verify(customer, params, customer_params)).to match(:get_vehicle)   
+  end
+	       
+  
+    
+
 end
 
